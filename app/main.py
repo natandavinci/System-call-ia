f043a7aff3e0f02be164b70131838a3b68418ee6
natandavinci <natanaelqueiroz969@gmail.com>
2026-05-30 09:58:52 -0300

    pagina inicial funcionando

--- app/main.py
+++ app/main.py
@@ -1,9 +1,14 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
 
+templates = Jinja2Templates(directory="app/templates")
+
+
 @app.get("/")
-def home():
-    return{
-        "message": "Hello World" #pra dá sorte rs
-    }
+async def home(request: Request):
+    return templates.TemplateResponse(
+        request=request,
+        name="index.html"
+    )
