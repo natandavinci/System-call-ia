c1c78deaaa1c8eef7bf25b247e07782540946eaf
natandavinci <natanaelqueiroz969@gmail.com>
2026-05-30 11:40:20 -0300

    adcionando a fala para a resposta do agente

--- app/static/js/voice.js
+++ app/static/js/voice.js
@@ -14,6 +14,27 @@ if (!ReconhecimentoFala){
 
     const reconhecimento = new ReconhecimentoFala()
 
+    //teste
+    reconhecimento.onstart = () => {
+    console.log("Microfone iniciado");
+    };
+
+    reconhecimento.onspeechstart = () => {
+        console.log("Usuário começou a falar");
+    };
+
+    reconhecimento.onspeechend = () => {
+        console.log("Usuário terminou de falar");
+    };
+
+    reconhecimento.onerror = (event) => {
+        console.log("ERRO COMPLETO:", event);
+        console.log("Código:", event.error);
+
+        status.innerText =
+            "Erro: " + event.error;
+    };
+
     reconhecimento.lang = "pt-BR";
 
     reconhecimento.continuous = false;
@@ -28,10 +49,11 @@ if (!ReconhecimentoFala){
     });
 
     reconhecimento.onresult = async (event) => {
-
+        console.log("1 - capturou áudio")
         const texto =
             event.results[0][0].transcript;
 
+        console.log("2 - capturou áudio")
         status.innerText = 
             "✅Texto capturado e enviando para o servidor"
 
@@ -53,11 +75,29 @@ if (!ReconhecimentoFala){
 
         );
 
+        console.log("3 - capturou áudio")
+
         const data = 
             await resposta.json();
 
+        console.log("4 - Json recebido")
+
         responseBox.innerText =
             data.response;
+
+        console.log("5 - Texto exibido");
+
+        const fala = new SpeechSynthesisUtterance(
+            data.response
+        );
+
+        fala.lang = "pt-BR";
+
+        console.log("6 - Objeto de fala criado");
+
+        speechSynthesis.speak(fala)
+
+        console.log("7 - falando");
 
         status.innerText = 
             "Resposta recebida";
