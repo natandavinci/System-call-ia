b8cb2dd05ab6e3b6f959bd88a88bdfb85f164d85
natandavinci <natanaelqueiroz969@gmail.com>
2026-05-30 10:09:37 -0300

    css adcionado

--- app/main.py
+++ app/main.py
@@ -1,7 +1,12 @@
 from fastapi import FastAPI, Request
 from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
 
 app = FastAPI()
+
+app.mount(
+    
+)
 
 templates = Jinja2Templates(directory="app/templates")
 
