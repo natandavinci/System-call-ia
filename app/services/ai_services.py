ccb3966188e5704ef3b1731041cc0098c16d014c
natandavinci <natanaelqueiroz969@gmail.com>
2026-05-30 11:47:11 -0300

    add memory

--- app/services/ai_services.py
+++ app/services/ai_services.py
@@ -8,11 +8,25 @@ client = OpenAI(
     api_key=os.getenv("OPEN_API_KEY")
 )
 
+historico = []
+
 def pergunte_ai(text: str):
-    
+
+    historico.append({
+        "role": "user",
+        "content": text
+    })
+
     resposta = client.responses.create(
         model="gpt-4.1-mini",
-        input = text
+        input = historico
     )
 
-    return resposta.output_text
+    responder = resposta.output_text
+
+    historico.append({
+        "role": "assistant",
+        "content": responder
+    })
+
+    return responder
