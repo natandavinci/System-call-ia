7a1b67e7c373496ca872ef73c973df004b352700
natandavinci <natanaelqueiroz969@gmail.com>
2026-06-06 09:14:50 -0300

    reestruturando o projeto

--- lang_test.py
+++ lang_test.py
@@ -5,3 +5,55 @@ print(
         "Quais os pacotes de viagens para o Brasil? me Dê as informações "
     )
 )
+
+'''PARA USAR DEPOIS NO AGENTE:
+from langchain_mcp_adapters.client import MultiServerMCPClient
+
+client = MultiServerMCPClient(
+    {
+        "travel_server": {
+                "transport": "streamable_http",
+                "url": "https://mcp.kiwi.com"
+            }
+    }
+)
+
+tools = await client.get_tools()
+
+
+
+
+from langchain.agents import create_agent
+from langgraph.checkpoint.memory import InMemorySaver
+
+agent = create_agent(
+    "gpt-5-nano",
+    tools=tools,
+    checkpointer=InMemorySaver(),
+    system_prompt="You are a travel agent. No follow up questions."
+)
+
+
+
+from langchain.messages import HumanMessage
+
+config = {"configurable": {"thread_id": "1"}}
+
+response = await agent.ainvoke(
+    {"messages": [HumanMessage(content="Ok talk aboutthe first option, explain everything")]},
+    config
+    )
+
+
+from pprint import pprint
+
+pprint(response)
+
+
+
+print(response["messages"][-1].content)
+
+
+
+
+ '''
